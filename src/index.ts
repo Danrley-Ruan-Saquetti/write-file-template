e084345d2577b91b96d64302c829b370c20d584f
Dan Ruan <101991292+Danrley-Ruan-Saquetti@users.noreply.github.com>
2023-06-09 18:02:49 +0000

    refatore to ts

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import { write } from "./lib/@esliph/wt";
+
+type NFArgs = { NOME_CLIENTE: string; VALOR: string };
+
+function gerarNotaFiscal({ NOME_CLIENTE, VALOR }: NFArgs) {
+  const args = {
+    NOME_CLIENTE,
+    VALOR,
+  };
+
+  const fileContent = write<NFArgs>(args, {
+    template: "public/templates/template_nota-fiscal.html",
+    dist: "public/templates/_nf",
+    fileName: `${new Date(Date.now()).getTime()}_nota_fiscal.html`,
+  });
+
+  return fileContent;
+}
+
+const resultFile = gerarNotaFiscal({
+  NOME_CLIENTE: "Dan Ruan",
+  VALOR: "1.000,00",
+});
+
+resultFile.getResult().error &&
+  console.log(resultFile.getResult().error.message);
---
+++ src/lib/@esliph/wt/index.ts
@@ -0,0 +1,45 @@
+import { Result } from "@esliph/util";
+import fs from "fs";
+
+export type WriteArgs<T extends object> = { [x in keyof T]: any };
+export type WriteFileConfig = {
+  template: string;
+  dist: string;
+  fileName: string;
+};
+
+export function write<T extends WriteArgs<T>>(
+  args: T,
+  fileConfig: WriteFileConfig
+) {
+  try {
+    let template = fs.readFileSync(fileConfig.template, "utf-8");
+
+    for (const key in args) {
+      if (!key) {
+        continue;
+      }
+
+      template = template.replace(`{{${key}}}`, args[key]);
+    }
+
+    fs.writeFileSync(`${fileConfig.dist}/${fileConfig.fileName}`, template, {
+      encoding: "utf-8",
+    });
+
+    return Result.success<string>(template);
+  } catch (err) {
+    return Result.failure<string>(
+      {
+        title: "Write Template",
+        message: [
+          {
+            message: err.message || "Cannot write template",
+            origin: err.path || fileConfig.template,
+          },
+        ],
+      },
+      400
+    );
+  }
+}
